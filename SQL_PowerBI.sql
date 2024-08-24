bccb742c644f8a41415719a33fc4ca4fc33977f4
Julio Vieira <juliostefano1327@gmail.com>
2024-08-24 11:46:09 -0300

    Correções de indentação

--- SQL_PowerBI.sql
+++ SQL_PowerBI.sql
@@ -38,8 +38,13 @@ SELECT
 	fis.TotalProductCost AS 'CUSTO VENDA',
 	fis.SalesAmount - fis.TotalProductCost AS 'LUCRO VENDA'
 FROM FactInternetSales fis
-INNER JOIN DimProduct dp ON fis.ProductKey = dp.ProductKey
-	INNER JOIN DimProductSubcategory dps ON dp.ProductSubcategoryKey = dps.ProductSubcategoryKey
-		INNER JOIN DimProductCategory dpc ON dps.ProductCategoryKey = dpc.ProductCategoryKey
-INNER JOIN DimCustomer dc ON fis.CustomerKey = dc.CustomerKey
-	INNER JOIN DimGeography dg ON dc.GeographyKey = dg.GeographyKey
+INNER JOIN DimProduct dp
+	ON fis.ProductKey = dp.ProductKey
+	INNER JOIN DimProductSubcategory dps
+		ON dp.ProductSubcategoryKey = dps.ProductSubcategoryKey
+		INNER JOIN DimProductCategory dpc
+			ON dps.ProductCategoryKey = dpc.ProductCategoryKey
+INNER JOIN DimCustomer dc
+	ON fis.CustomerKey = dc.CustomerKey
+	INNER JOIN DimGeography dg 
+		ON dc.GeographyKey = dg.GeographyKey

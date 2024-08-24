c0816fcfb8dfa2ee463dcd1def68d6c33dc47ebe
Julio Vieira <juliostefano1327@gmail.com>
2024-08-24 11:28:15 -0300

    Definindo escopo do desenvolvimento

---
+++ SQL_PowerBI.sql
@@ -0,0 +1,22 @@
+ 
+ --PROJETO DE INTEGRAÇÃO SQL SERVER e POWER BI
+
+-- 1. Definindo os indicadores do projeto
+
+-- ABA GERAL
+
+-- i) Receita Total
+-- ii) Quantidade Vendida
+-- iii) Total de Categorias de Produtos
+-- iv) Quantidade de Clientes
+-- v) Receita Total e Lucro Total por Mês
+-- vi) Margem de Lucro
+-- vii) Quantidade Vendida por Mês
+-- viii) Lucro por País
+
+-- ABA CLIENTES
+
+-- i) Vendas por País
+-- ii) Clientes por País
+-- iii) Vendas por Gênero
+-- iv) Vendas por Categoria
